20a26841626929bdab871c3e0c6aa8bf935625c0
Jimmy Yeung <jimmy090804@gmail.com>
2020-10-05 23:51:37 +0800

    update comment on 71

--- leetcode/medium/71-simplify-path/main.swift
+++ leetcode/medium/71-simplify-path/main.swift
@@ -6,6 +6,9 @@
  In a UNIX-style file system, a period . refers to the current directory. Furthermore, a double period .. moves the directory up a level.
 
  Note that the returned canonical path must always begin with a slash /, and there must be only a single slash / between two directory names. The last directory name (if it exists) must not end with a trailing /. Also, the canonical path must be the shortest string representing the absolute path.
+ 
+ Time complexity: O(n)
+ Space complexity: O(2n)
  */
 class Solution {
     func simplifyPath(_ path: String) -> String {
